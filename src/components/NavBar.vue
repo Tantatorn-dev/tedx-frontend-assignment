0e15492da99c8f4365ae3c0494a2fe95a4598ff1
Tantatorn <timefear007@gmail.com>
2021-04-27 01:57:18 +0700

    add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar :shadow="true">
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <Logo />
+      </b-navbar-item>
+    </template>
+
+    <template #end>
+      <b-navbar-item href="#"> Find A Match </b-navbar-item>
+      <b-navbar-item href="#"> Dog's Personalities </b-navbar-item>
+      <b-navbar-item href="#"> About Us </b-navbar-item>
+      <b-navbar-item href="#"> Contact Us </b-navbar-item>
+      <b-navbar-item href="#">
+        <img src="../assets/loupe.png" />
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+import Logo from "./Logo";
+
+export default {
+  name: "NavBar",
+  components: {
+    Logo,
+  },
+};
+</script>
