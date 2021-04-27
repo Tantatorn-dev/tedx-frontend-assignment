977a5bc6ed5bafa8a6ef5cd6b732c0ac871c4ee2
Tantatorn <timefear007@gmail.com>
2021-04-27 14:42:54 +0700

    styling main section

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -4,10 +4,10 @@
       <img id="doggo" src="../assets/hero.png" />
     </div>
     <div class="column">
-      <h1 id="topo1" class="is-size-1">
+      <h1 id="topo1" class="is-size-4 has-text-weight-semibold">
         What's meant to be will always find a way.
       </h1>
-      <h1 id="topo2" class="is-size-4">
+      <h1 id="topo2" class="is-size-6">
         PawsUp help you to find the best <br />
         partner for your dearest fluffy friend
       </h1>
@@ -31,8 +31,9 @@ export default {
 
 <style scoped>
 #topo1 {
-  margin: 100px;
+  margin-inline: 100px;
   margin-bottom: 10px;
+  margin-top: 100px;
 }
 #match-button {
   margin-top: 20px;
@@ -56,6 +57,11 @@ export default {
 
 .reverse-row-order {
   flex-direction: row-reverse;
+}
+
+@media screen and (max-width:400px) {
+    #topo1{
+        margin-top: 0px;}
 }
 
 </style>
